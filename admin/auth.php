29203c5641c1ed058710c0ff815b58f2e0d6273e
Mike Rushton <mike@mikerushton.com>
2026-02-21 22:20:23 -0500

    comma separated list of ip's, CIDR's or partial ip

--- admin/auth.php
+++ admin/auth.php
@@ -1,12 +1,46 @@
 <?php
 session_start();
 
-$allowed_subnet = '192.168.1.'; 
-$password = '{{ auth_password }}';
+$password = '{{ 3dps_auth_password }}';
+$ip_allowlist = explode(',', '{{ 3dps_allowed_ips }}');
+$ip_allowlist = array_map('trim', $ip_allowlist);
+$client_ip = $_SERVER['REMOTE_ADDR'];
 
-#if (!str_starts_with($_SERVER['REMOTE_ADDR'], $allowed_subnet)) {
-#    die('Access denied (not on local network)');
-#}
+function ip_allowed($client_ip, $allowlist) {
+
+    foreach ($allowlist as $rule) {
+
+        // CIDR
+        if (strpos($rule, '/') !== false) {
+            [$subnet, $mask] = explode('/', $rule);
+
+            if ((ip2long($client_ip) & ~((1 << (32 - $mask)) - 1)) == ip2long($subnet)) {
+                return true;
+            }
+        }
+
+        // exact match
+        if ($client_ip === $rule) {
+            return true;
+        }
+
+        // prefix match (partial IP like 10.0.0.)
+        if (str_ends_with($rule, '.') && str_starts_with($client_ip, $rule)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+if (ip_allowed($client_ip, $ip_allowlist)) {
+    $_SESSION['logged_in'] = true;
+    header('Location: index.php');
+    exit;
+}else {
+    die('Access denied (not on local network)');
+}
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['password'] === $password) {
@@ -22,4 +56,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <input type="password" name="password" placeholder="Password">
     <button type="submit">Login</button>
 </form>
-
